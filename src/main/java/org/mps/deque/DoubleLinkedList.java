ffa1e0ae21d53303694f9db71f32a6cd562f70c7
Cano <clementecano2003@gmail.com>
2024-03-19 11:54:45 +0100

    Update DoubleLinkedList.java

--- src/main/java/org/mps/deque/DoubleLinkedList.java
+++ src/main/java/org/mps/deque/DoubleLinkedList.java
@@ -14,39 +14,86 @@ public class DoubleLinkedList<T> implements DoubleLinkedQueue<T> {
 
     public DoubleLinkedList() {
         // TODO
-        
+        this.first = null;
+        this.last = null;
+        this.size = 0;
     }
 
     @Override
     public void prepend(T value) {
         // TODO
+        LinkedNode<T> newNode = new LinkedNode<T>(value, null, null);
+        if (size==0) {
+            first = newNode;
+            last = null;
+        } else {
+            newNode.setNext(first);
+            first.setPrevious(newNode);
+            first = newNode;
+        }
+
+        size++;
     }
 
     @Override
     public void append(T value) {
         // TODO
+        LinkedNode<T> newNode = new LinkedNode<T>(value, null, null);
+        if(size == 0){
+            last = newNode;
+            first = null;
+        }else{
+            newNode.setPrevious(last);
+            last.setNext(newNode);
+            last = newNode;
+        }
     }
 
     @Override
     public void deleteFirst() {
         // TODO
+        LinkedNode<T> newNode = new LinkedNode<T>(null, null, null);
+        if(size == 0){
+            throw new DoubleLinkedQueueException("Lista Vacia: no se puede borrar");
+        }else{
+            newNode = first.getNext();
+            first = newNode;
+
+        }
     }
 
     @Override
     public void deleteLast() {
-        // TODO
+        // 
+        LinkedNode<T> newNode = new LinkedNode<T>(null, null, null);
+        if(size == 0){
+            throw new DoubleLinkedQueueException("Lista Vacia: no se puede borrar");
+        }else{
+            newNode = last.getPrevious();
+            last = newNode;
+
+        }
     }
 
     @Override
     public T first() {
         // TODO
-        return first.getItem();
+        if(size == 0){
+            throw new DoubleLinkedQueueException("Lista Vacia: no se puede devolver el primero");
+        }else{
+            return (T) first;
+        }
     }
 
     @Override
     public T last() {
         // TODO
-        return last.getItem();
+        if(size == 0){
+            throw new DoubleLinkedQueueException("Lista Vacia: no se puede devolver el ultimo");
+        }else{
+            return (T) last;
+        }
+        
     }
 
     @Override
