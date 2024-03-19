b242f5979b32459e9f755022e79ed2826219cb24
Cano <clementecano2003@gmail.com>
2024-03-19 19:39:57 +0100

    Update DoubleLinkedListTest.java

--- src/test/java/org/mps/deque/DoubleLinkedListTest.java
+++ src/test/java/org/mps/deque/DoubleLinkedListTest.java
@@ -1,27 +1,86 @@
 package org.mps.deque;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class DoubleLinkedListTest {
-    DoubleLinkedList list;
-
-    @BeforeEach
-    void setUp_list(){
-        list = new DoubleLinkedList<>();
-    }
-    
     @Nested
-    @DisplayName("Pruebas al método first")
-    class testFirst {
-
+    @DisplayName("Tests para probar el constructor")
+    class testConstructor{
         @Test
-        @DisplayName("First con una lista con elementos devuelve el primero")
-        void first_whenThereAreElements_worksCorrectly() {
+        @DisplayName("Con los parámetros adecuados")
+        void constructorWithGoodParameter(){
+            DoubleLinkedList <Integer> list = new DoubleLinkedList<>();
+
+            assertNull(list.first());
+            assertNull(list.last());
+            assertEquals(0, list.size());
+
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests para probar el metodo prepend")
+    class testPrepend{
+        @Test
+        @DisplayName("Comprobamos que añada correctamente 1 al principio")
+        void prependOneElementAtTheBegin() {
+            DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+
+            list.prepend(1);
             
+            assertEquals(1, list.size());
+            assertEquals(1, list.first());
+            assertEquals(1, list.last());
         }
 
+        @Test
+        @DisplayName("Comprobamos que añada correctamente 3 nodos al principio")
+        void prependThreeElementAtTheBegin() {
+            DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+
+            list.prepend(1);
+            list.prepend(2);
+            list.prepend(3);
+            
+            assertEquals(3, list.size());
+            assertEquals(3, list.first());
+            assertEquals(1, list.last());
+        }
     }
+
+    @Nested
+    @DisplayName("Tests para probar el metodo append")
+    class testAppend{
+        @Test
+        @DisplayName("Comprobamos que añada correctamente 1 elemento al final")
+        void appendOneElementAtTheEnd() {
+            DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+
+            list.append(1);
+            
+            assertEquals(1, list.size());
+            assertEquals(1, list.first());
+            assertEquals(1, list.last());
+        }
+
+        @Test
+        @DisplayName("Comprobamos que añada correctamente 3 nodos al final")
+        void appendThreeElementAtTheEnd() {
+            DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+
+            list.append(1);
+            list.append(2);
+            list.append(3);
+            
+            assertEquals(3, list.size());
+            assertEquals(1, list.first());
+            assertEquals(3, list.last());
+        }
+    }
+    
 }
